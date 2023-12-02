ac83753c8b634d943957ccad10e9dd42a0eb89d8
Ishan Ranjan <ishanranjan040@gmail.com>
2023-12-01 19:07:36 -0500

    made contacts viewable on contacts page.

--- resources/views/contacts/contacts.blade.php
+++ resources/views/contacts/contacts.blade.php
@@ -8,12 +8,23 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if($contacts->isEmpty())
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     You do not have any contacts.
                 </div>
             </div>
-<br><br>
+
+    @else
+        <ul>
+            @foreach($contacts as $contact)
+                <li>{{ $contact->name }} - {{ $contact->email }} - {{ $contact->contact }} - {{ $contact->address }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <br><br>
+
         <a  href="contacts/create">
             <div style="margin:auto; width: 50%;" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div style="text-align:center;" class="p-6 text-gray-900 dark:text-gray-100">
