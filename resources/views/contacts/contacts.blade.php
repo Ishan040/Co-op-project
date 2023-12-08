605558a7f777374e8f149236608e0b56c362c1d5
Ishan Ranjan <ishanranjan040@gmail.com>
2023-12-07 19:35:31 -0500

    made changes to contact ui.

--- resources/views/contacts/contacts.blade.php
+++ resources/views/contacts/contacts.blade.php
@@ -21,14 +21,14 @@
         @if($contacts->count() > 0)
             @foreach($contacts as $contact)
 
-            <div style="margin-bottom: 20px; padding: 10px; border: 1px solid #ccc; border-radius: 5px;">
+            <div style="margin-bottom: 20px; padding: 10px; border: 1px solid #ccc; border-radius: 5px; position:relative;">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <strong>Name: </strong>{{ $contact->name }}<br>
                         <strong>Email: </strong>{{ $contact->email }}<br>
                         <strong>Contact: </strong>{{ $contact->contact }}<br>
                         <strong>Address: </strong>{{ $contact->address }}<br>
-                        <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-primary">Edit Contact</a>
+                        <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-primary" style="position: absolute; top: 50%; transform: translateY(-50%); right: 20px; padding: 5px; border: 1px solid #ccc; border-radius: 5px">Edit Contact</a>
                     </div>
                 </div>
             </div>
