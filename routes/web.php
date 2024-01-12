364fee0f9d40470e5d8f44c65d5bac49b9d0622d
Ishan Ranjan <ishanranjan040@gmail.com>
2024-01-11 19:28:22 -0500

    fixed login page error

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', function () {
     return view('home');
-});
+})->middleware('auth');
 
 Route::get('/', function () {
     return view('home');
-});
+})->middleware('auth');
 
 Route::get('contacts', function () {
     return view('contacts/contacts');
